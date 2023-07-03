362d748a0e51ec59561300236fc8e1ae848dab79
chaem <chaem12@ex-em.com>
2023-07-03 17:38:33 +0900

    feat: 거래 내역 페이지 구현

--- src/components/Contents/TransactionContents.vue
+++ src/components/Contents/TransactionContents.vue
@@ -1,6 +1,30 @@
 <template>
-<h2>거래내역 페이지</h2>
+    <h2>거래내역 페이지</h2>
+    <div  v-for="transaction of transactionList" :key="transaction.id" >
+        <card-view>
+            <div>
+                {{transaction.date}} {{ transaction.sum }}
+            </div>
+            <div v-for="item of transaction.items" :key="item.id">
+                <strong>{{item.name}}</strong>
+                {{item.num}}
+            </div>
+        
+
+        </card-view>
+    </div>
 </template>
+
+<script setup>
+import CardView from '../../common/CardView.vue';
+
+const transactionList = JSON.parse(localStorage.getItem('transaction'))||[];
+
+
+
+
+</script>
+
 <style scoped>
 h2{
     margin: 0;
